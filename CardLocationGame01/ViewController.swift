b47e667ae5fc3f4683ce3adfe9a2307d6463ce31
shani shalev <shanishalev26@gmail.com>
2026-05-21 12:11:02 +0300

    Initial Commit

---
+++ CardLocationGame01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardLocationGame01
+//
+//  Created by שני שלו on 21/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
